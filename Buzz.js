19dc41e83fa7cc5f51b8a4aa5d7e6a7b78163af7
Ernie Massei <Emassei05@gmail.com>
2014-04-28 14:13:37 -0700

    Initial upload
    Beginnings

---
+++ Buzz.js
@@ -0,0 +1,21 @@
+
+
+
+var answer = prompt("What is your favorite number?");
+
+for(i=1;i<=answer;i++){
+
+  if(i%3==0 && i%5==0){
+    document.write("FizzBuzz")
+  }
+  else if(i%3==0){
+    document.write("Fizz");
+  }
+  else if (i%5==0){
+    document.write("Buzz");
+  }
+  else{
+    document.write(i);
+  }
+   document.write("<br>");
+}
